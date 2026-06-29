fc36d2f55a0e9b1e6f8334a6829f5452d0281a8c
huluhuqi <475283077@qq.com>
2026-06-29 11:50:40 +0800

    fix: 修复timeSlot导入大小写问题(Linux CI构建失败)

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export { CategoryAPI } from './category'
 export { InstrumentAPI } from './instrument'
 export { BookingAPI } from './booking'
-export { TimeSlotAPI } from './timeslot'
+export { TimeSlotAPI } from './timeSlot'
 export { UserAPI } from './user'
